2abd8b0c50a8d052d23953fd030d127420484e17
Aluízio Catão <aluizio2210@gmail.com>
2022-04-27 19:34:04 -0500

    Aula 3 Parte 1

---
+++ src/main/java/br/ufac/sgcmapi/service/AtendimentoService.java
@@ -0,0 +1,48 @@
+package br.ufac.sgcmapi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.ufac.sgcmapi.model.Atendimento;
+import br.ufac.sgcmapi.model.EStatusAtendimento;
+import br.ufac.sgcmapi.repository.AtendimentoRepository;
+
+public class AtendimentoService implements ICrudService<Atendimento>{
+
+    private final AtendimentoRepository repo;
+
+    @Autowired    
+    public AtendimentoService(AtendimentoRepository repo){
+        this.repo = repo;
+    }
+
+    @Override
+    public List<Atendimento> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Atendimento getById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Atendimento> getByAll(String termoBusca) {
+        return repo.findByAll(termoBusca);
+    }
+
+    @Override
+    public Atendimento save(Atendimento objeto) {
+        return repo.save(objeto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Atendimento registro = repo.getById(id);
+        registro.setStatus(EStatusAtendimento.CANCELADO);
+        repo.save(registro);
+    }
+    
+
+}
